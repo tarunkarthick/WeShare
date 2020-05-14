b36e5551a841c796d990bbbe7bfc236882034aba
tarunkarthick <tarunkarthick.7@gmail.com>
2020-05-15 01:46:45 +0530

    commit

--- server.js
+++ server.js
@@ -66,10 +66,9 @@ app.use(function (err, req, res, next) {
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build')); // serve the static react app
-    app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
-      res.sendFile(path.join(__dirname, './client/build/index.html'));
-    });
-    console.log('Serving React App...');
+    app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
   };
 
 
