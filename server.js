a996be092e74b697d7e2f67d99c7c9756df66845
tarunkarthick <54742238+tarunkarthick@users.noreply.github.com>
2023-07-20 20:26:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use("/",userRoutes)
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
+    res.redirect('https://' + req.hostname + req.url);
   } else {
     next();
   }
